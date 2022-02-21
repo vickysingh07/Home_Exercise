83d4ca3a939018fb40359a6fce774d32edc4300a
Vicky Singh <vicky1910098@akgec.ac.in>
2022-02-22 00:45:09 +0530

    second commit

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,109 +1,97 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, file_names
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:home_exercise/Screens/CustomAppBar.dart';
+
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _colorTween, _homeTween, _yogaTween, _iconTween, _drawerTween;
+  late AnimationController _textAnimationController;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 0));
+    _colorTween = ColorTween(begin: Colors.transparent, end: Colors.white)
+        .animate(_animationController);
+    _iconTween = ColorTween(begin: Colors.white, end: Colors.lightBlue)
+        .animate(_animationController);
+    _drawerTween = ColorTween(begin: Colors.white, end: Colors.black)
+        .animate(_animationController);
+    _homeTween = ColorTween(begin: Colors.white, end: Colors.blue)
+        .animate(_animationController);
+    _yogaTween = ColorTween(begin: Colors.white, end: Colors.black)
+        .animate(_animationController);
+    _textAnimationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 0));
+    super.initState();
+  }
+
+  bool scrollListner(ScrollNotification scrollNotification) {
+    bool scroll = false;
+    if (scrollNotification.metrics.axis == Axis.vertical) {
+      _animationController.animateTo(scrollNotification.metrics.pixels / 80);
+      _textAnimationController.animateTo(scrollNotification.metrics.pixels);
+      return scroll = true;
+    }
+    return scroll;
+  }
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer: Drawer(),
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const  Text("Home Exercise"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+      body: NotificationListener(
+        onNotification: scrollListner,
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: const[
-                      Text("1"),
-                      Text("Streak")
-                    ],
-                  ),
-                  Column(
-                    children: const[
-                      Text("1"),
-                      Text("Streak")
-                    ],
-                  ),
-                  Column(
-                    children: const[
-                      Text("1"),
-                      Text("Streak")
-                    ],
-                  )
-
-                ],
-              ),
-            ),
-            const Divider(thickness: 3,indent: 20, endIndent: 20,),
+            // ignore: sized_box_for_whitespace
             Container(
-              padding: const EdgeInsets.all(25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              height: double.infinity,
+              child: Stack(
                 children: [
-                  const Text("Yoga For All"),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-
+                  SingleChildScrollView(
+                    child: Stack(
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              height: 1000,
+                              color: Colors.blue,
+                            ),
+                            Container(
+                              height: 1000,
+                              color: Colors.green,
+                            ),
+                            Container(
+                              height: 1000,
+                              color: Colors.blue,
+                            )
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Exercise For Students"),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-
-                    ),
-                  )
+                  CustomAppBar(
+                      animationController: _animationController,
+                      colorsTween: _colorTween,
+                      drawerTween: _drawerTween,
+                      homeTween: _homeTween,
+                      iconTween: _iconTween,
+                      onPressed: () {
+                        scaffoldKey.currentState?.openDrawer();
+                      },
+                      yogaTween: _yogaTween)
                 ],
               ),
             )
